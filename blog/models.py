7b059bf66239e29b2a9025e9b4a7d1fa275d53bd
specialjin1 <specialjin1@gmail.com>
2018-05-01 22:59:47 +0900

    real last

--- blog/models.py
+++ blog/models.py
@@ -11,8 +11,11 @@ class Post(models.Model) : # Object Declaration
 		
 		def publish(self):
 			self.published_date = timezone.now()
-			self.save()
-
+			self.save()	
+		
+		def approved_comments(self):
+			return self.comments.filter(approved_comment=True)
+		
 		def __str__(self):
 			return self.title
 
@@ -26,9 +29,6 @@ class Comment(models.Model):
 	def approve(self):
 		self.approved_comment = True
 		self.save()
-	
-	def approved_comments(self):
-		return self.comment.filter(approved_comment=True)
 	
 	def __str__(self):
 		return self.text
